440379ec2b99abbfba4d565eab1117deea88ba35
Mahmood <medoysf@gmail.com>
2018-05-31 16:18:01 +0200

    first time

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.post('/', (req,res)=> {
+	const body = req.body;
+	if(body.queryResult.parameters['geo-city']){
+		let city = body.queryResult.parameters['geo-city'];
+		res.json({'fullfilment' : city});
+	}
+}) ;
+
+app.listen(process.env.port || 8080 , function(err){
+	if(err){
+		throw err;
+	}
+	console.log('Server started on Port : ' + process.env.port);
+});
