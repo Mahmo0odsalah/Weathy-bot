51e4821592bb79d89b8e215033c2b870c75245ad
Mahmood <medoysf@gmail.com>
2018-05-31 23:28:09 +0200

    city

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const http = require('http');
+const host = 'api.worldweatheronline.com';
+const wwoApiKey = "";
 const app = express();
 
 app.use(bodyParser.json());
@@ -10,9 +12,61 @@ app.use(bodyParser.json());
 app.post('/', (req,res)=> {
 	res.set('Content-type','application/json');
 	var body = req.body;
-	let city = body.queryResult.parameters['geo-city'];
-	res.json({'fulfillmentText' : city});
+	if(body.queryResult.parameters['geo-city']){
+		let city = body.queryResult.parameters['geo-city'];
+		callWeatherApi(city).then((output) => {
+    		res.json({ 'fulfillmentText': output });
+  		}).catch(() => {
+    		res.json({ 'fulfillmentText': `Couldn't get the weather :(` });
+		});
+	}
+
+	if(body.queryResult.parameters['Latitude']){
+		const lat = body.queryResult.parameters['Latitude'];
+		const long = body.queryResult.parameters['Longitude'];
+
+		callWeatherApi(lat , long).then((ouput)=>{
+			res.json({ 'fulfillmentText': output });
+  		}).catch(() => {
+    		res.json({ 'fulfillmentText': `Couldn't get the weather :(` });
+		})
+		}
 	}) ;
+
+
+function callWeatherApi(city) {
+	return new Promise((resolve,reject) =>{
+		let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +'&q=' + encodeURIComponent(city);
+		 http.get({host: host, path: path}, (res) => {
+      let body = ''; // var to store the response chunks
+      res.on('data', (d) => { body += d; }); // store each response chunk
+      res.on('end', () => {
+        // After all the data has been received parse the JSON for desired data
+        let response = JSON.parse(body);
+        let forecast = response['data']['weather'][0];
+        let location = response['data']['request'][0];
+        let conditions = response['data']['current_condition'][0];
+        let currentConditions = conditions['weatherDesc'][0]['value'];
+
+        // Create response
+        let output = `Current conditions in the ${location['type']} 
+        ${location['query']} are ${currentConditions} with a projected high of
+        ${forecast['maxtempC']}°C or ${forecast['maxtempF']}°F and a low of 
+        ${forecast['mintempC']}°C or ${forecast['mintempF']}°F on 
+        ${forecast['date']}.`;
+
+        // Resolve the promise with the output text
+        console.log(output);
+        resolve(output);
+      });
+      res.on('error', (error) => {
+        console.log(`Error calling the weather API: ${error}`)
+        reject();
+      });
+    });
+  });
+}
+	
 
 app.listen(process.env.PORT || 8080 , function(err){
 	if(err){
